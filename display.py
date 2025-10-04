70ba01c93f6a29db46977e45f783e9b9bf3219dd
MartinSchwald <martin.schwald@uni-due.de>
2025-10-04 12:02:58 +0200

    restructure images folder

---
+++ display.py
@@ -0,0 +1,43 @@
+import pygame
+import settings
+
+class Display:
+    # Go into fullscreen mode with the current display settings of the player
+    def __init__(self):
+        pygame.display.init()
+        self.info = pygame.display.Info()
+        self.width, self.height = self.info.current_w, self.info.current_h
+        self.display = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
+
+    def get_game_surface_with_ratio(self,w,h):
+        #Fix a maximal surface "screen" on the display with following width:height ratio
+        ratio = h*self.width-w*self.height
+
+        if ratio == 0:
+            # display has the desired ratio, no padding necessary
+            screen_w,screen_h = (self.width,self.height)
+            self.padding_w, self.padding_h = (0,0)  
+        elif ratio > 0:
+            # display is too wide
+            screen_w, screen_h = w * self.height // h, self.H
+            self.padding_w, self.padding_h = (self.width - screen_w) // 2, 0
+        else:
+            # display is too high
+            screen_w, screen_h = self.width, self.height * h // w
+            self.padding_w, self.padding_h = 0, (self.height - screen_h) // 2
+
+        self.screen = pygame.Surface((screen_w,screen_h))
+        self.screen_rect = pygame.Rect(self.padding_w,self.padding_h,screen_w,screen_h)
+        Display.screen_width = self.screen.get_width()
+        Display.screen_height = self.screen.get_height()
+        Display.grid_width = Display.screen_width // settings.grid[0]
+        return self.screen
+
+    def update(self,padding_color):
+        """blits screen centered on display,
+                                padding visible if screen ratio is not as fixed
+                                in the settings"""
+        self.display.fill(padding_color)
+        self.display.blit(self.screen,self.screen_rect)
+        pygame.display.flip()
+
---
+++ text.py
@@ -0,0 +1,130 @@
+import pygame
+from pygame.locals import *
+import settings
+from settings import color
+import sound
+from image import Image
+
+def pad_surface(surface, padding_size, padding_color=None, vertical_padding=True, horizontal_padding=True):
+    x,y = 0,0
+    if vertical_padding:
+        y+=padding_size
+    if horizontal_padding:
+        x+=padding_size
+    result = pygame.Surface((surface.get_width()+2*x,surface.get_height()+2*y))
+    if padding_color is not None:
+        result.fill(padding_color)
+    result.blit(surface,(x,y))
+    return result
+
+def align_surfaces(surfaces, orientation, alignment = "center", rescale_to_surface = None, spacing = 0, padding_size = 0, padding_color = None):
+    """aligns a list of pygame surfaces, orientation can either be "horizontal" or "vertical",
+    alignment can either be "center" and "left","right" or "top","bottom" resp.
+    alternativeley, surfaces can also be rescaled to a given surface or size
+    to align perfectly"""
+    if surfaces == []:
+        return pygame.Surface((0,0))
+
+    if rescale_to_surface is not None:
+        (w0,h0) = rescale_to_surface.get_size()
+        if orientation == "horizontal":
+            surfaces = [pygame.transform.scale_by(surface,h0/surface.get_height()) for surface in surfaces]
+            h=h0
+        elif orientation == "vertical":
+            surfaces = [pygame.transform.scale_by(surface,w0/surface.get_width()) for surface in surfaces]
+            w=w0
+    else:
+        if orientation == "horizontal":
+            h = max(surface.get_height() for surface in surfaces)
+        elif orientation == "vertical":
+            w = max(surface.get_width() for surface in surfaces)
+
+    if orientation == "horizontal":
+        w = sum([surface.get_width() for surface in surfaces])  
+        result = pygame.Surface((w+(len(surfaces)-1)*spacing,h), pygame.SRCALPHA)
+        x,y = 0,0
+        for surface in surfaces:
+            if rescale_to_surface is not None or alignment == "top":
+                y = 0
+            elif alignment == "center":
+                y=(h-surface.get_height())/2
+            elif alignment == "bottom":
+                y=h-surface.get_height()
+            result.blit(surface,(x,y))
+            x += surface.get_width() + spacing
+    elif orientation == "vertical":
+        h = sum([surface.get_height() for surface in surfaces])
+        result = pygame.Surface((w,h+(len(surfaces)-1)*spacing), pygame.SRCALPHA)
+        x,y = 0,0
+        for surface in surfaces:
+            if rescale_to_surface is not None or alignment == "left":
+                x = 0
+            elif alignment == "center":
+                x=(w-surface.get_width())/2
+            elif alignment == "right":
+                x=w-surface.get_width()
+            result.blit(surface,(x,y))
+            y += surface.get_height() + spacing
+
+    return pad_surface(result,padding_size, padding_color)
+
+def render_line(line, font, text_color, bg_color):
+    """For line either a string or a list of strings and surfaces
+    render them as a single text line"""
+    if isinstance(line, str):
+        return font.render(line, False, text_color, bg_color)
+    else:
+        h = font.get_height()
+        rendered_line_parts = []
+        for part in line:
+            if isinstance(part, str):
+                rendered_line_parts.append(font.render(part, False, text_color, bg_color))
+            else:
+                rendered_line_parts.append(pygame.transform.scale_by(part,h/part.get_height()))
+        return align_surfaces(rendered_line_parts, "horizontal", padding_color = bg_color)
+
+
+    if rescale_to_size is not None:
+        if orientation == "horizontal":
+            h=rescale_to_size
+            surfaces = [pygame.transform.scale_by(surface,h/surface.get_height()) for surface in surfaces]
+        elif orientation == "vertical":
+            w=rescale_to_size
+            surfaces = [pygame.transform.scale_by(surface,w/surface.get_width()) for surface in surfaces]
+
+
+class Text:
+    """class to render text with a given layout"""
+    def __init__(self, lines, font, text_color, bg_color=None):
+        """for a list "lines" of strings (and surfaces) render its lines as surfaces
+        that can be composed to a bigger surface with given alignment."""
+        self.lines = lines
+        self.font = font
+        self.text_color = text_color
+        self.bg_color = bg_color
+        self.rendered_lines = [render_line(line, font, text_color, bg_color) for line in lines]
+        self.max_line_length = max(line.get_width() for line in self.rendered_lines)
+        self.title_length = self.rendered_lines[0].get_width()
+
+    def render(self, alignment, line_distance, padding_size=0, padding_color=None, center_title=True, title_distance=None):
+        if padding_color is None:
+            padding_color = self.bg_color
+        if title_distance is None:
+            title_distance = line_distance
+        if len(self.lines) == 1:
+            return pad_surface(self.rendered_lines[0], padding_size, padding_color)
+        if center_title:
+            rendered_text_body = align_surfaces(self.rendered_lines[1:], orientation="vertical",
+                            alignment = alignment, spacing = line_distance, padding_size = 0, padding_color = self.bg_color)
+            
+            if self.rendered_lines[0].get_width() < self.max_line_length:
+                rendered_text = align_surfaces([self.rendered_lines[0],rendered_text_body], orientation="vertical",
+                            alignment = "center", spacing = title_distance, padding_size = 0, padding_color = self.bg_color)
+            else:
+                rendered_text = align_surfaces([self.rendered_lines[0],rendered_text_body], orientation="vertical",
+                            alignment = alignment, spacing = title_distance, padding_size = 0, padding_color = self.bg_color)
+
+        else:
+            rendered_text = align_surfaces(self.rendered_lines, orientation="vertical",
+                            alignment = alignment, spacing = line_distance, padding_size = 0, padding_color = self.bg_color)
+        return pad_surface(rendered_text, padding_size, padding_color)
